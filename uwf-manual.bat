25fd85849092dcc4d0dee33086a13634bd4bd458
danez13 <hernandezdaniel170@gmail.com>
2024-10-25 07:38:20 -0400

    UPDATE uwf-manual.bat

--- uwf-manual.bat
+++ uwf-manual.bat
@@ -29,39 +29,53 @@ if '%errorlevel%' NEQ '0' (
     CD /D "%~dp0"
 :--------------------------------------
 
-setlocal enabledelayedexpansion enabledelayedexpansion
-
+setlocal enabledelayedexpansion
 set "searchString=Enabled"
+choice /C EDRX /M "Do you want to Enable UWF, Disable UWF, Reboot the system, or exit?"
+if %errorlevel% equ 1 (
+    choice /c YN /M "are you sure you want to enable UWF"
+    if %errorlevel% equ 2 (
+        :: echo determining if Device Lockdown feature is enabled...
+        for /f "delims=" %%i in ('Dism /online /Get-Features /format:table ^| find "Client-DeviceLockdown"') do (
+            set "output=!output! %%i"
+        )
+        echo !output! > temp.txt
 
-echo determining if Device Lockdown feature is enabled...
-for /f "delims=" %%i in ('Dism /online /Get-Features /format:table ^| find "Client-DeviceLockdown"') do (
-    set "output=!output! %%i"
+        findstr /C:"%searchString%" temp.txt > nul
+
+        if %errorlevel% equ 1 (
+            echo enabling Device Lockdown feature...
+            DISM /Online /Enable-Feature /FeatureName:Client-DeviceLockdown
+        ) else (
+            echo Device Lockdown is enabled
+        )
+
+        @REM echo determining if Unified Write Filter feature is enabled...
+        for /f "delims=" %%i in ('Dism /online /Get-Features /format:table ^| find "Client-UnifiedWriteFilter"') do (
+            set "output=!output! %%i"
+        )
+        echo !output! > temp.txt
+
+        findstr /C:"%searchString%" temp.txt > nul
+
+        if %errorlevel% equ 1 (
+            echo enabling Unified Write Filter feature...
+            DISM /Online /Enable-Feature /FeatureName:Client-UnifiedWriteFilter
+        ) else (
+            echo Unified Write Filter feature is enabled
+        )
+    ) else if %errorlevel% equ 3 (
+        call uwf-manual.bat
+        exit /b
+    )
+) else if errorlevel 2 (
+    echo 2
 )
-echo !output! > temp.txt
-
-findstr /C:"%searchString%" temp.txt > nul
-
-if errorlevel 1 (
-    echo enabling Device Lockdown feature...
-    DISM /Online /Enable-Feature /FeatureName:Client-DeviceLockdown
-) else (
-    echo Device Lockdown is enabled
+if errorlevel 3 (
+    echo 3
 )
-
-echo determining if Unified Write Filter feature is enabled...
-for /f "delims=" %%i in ('Dism /online /Get-Features /format:table ^| find "Client-UnifiedWriteFilter"') do (
-    set "output=!output! %%i"
+if errorlevel 4 (
+    echo 4
 )
-echo !output! > temp.txt
-
-findstr /C:"%searchString%" temp.txt > nul
-
-if errorlevel 1 (
-    echo enabling Unified Write Filter feature...
-    DISM /Online /Enable-Feature /FeatureName:Client-UnifiedWriteFilter
-) else (
-    echo Unified Write Filter feature is enabled
-)
-endlocal
-
 del temp.txt
+endlocal
